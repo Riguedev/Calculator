4597c955d89caf480417b50d0eac87583bcffb79
Jesús Rico <riguedev@gmail.com>
2024-02-09 22:24:24 -0600

    Agregue la funcionalidad de eliminar de uno en uno y todos los elementos en la pantalla

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 const keys =  document.getElementsByClassName('number')
 const screen = document.getElementById('screen')
+const deletKey = document.getElementById('D')
+const deleteAll = document.getElementById('C')
 let screenNumbers = []
-
-/*
-    Es la primera prueba de agregar datos a la pantalla
-
-for (let i = 0; i < keys.length; i++) {
-    keys[i].addEventListener('click', () => {
-        const newNumberScreen = document.createElement('span')
-        newNumberScreen.innerText = keys[i].innerHTML
-        screen.appendChild(newNumberScreen)
-
-        console.log(screen.length)
-    })
-}*/
 
 const addNumber = () => {
     const number = document.createElement('span')
+    number.classList.add('delete')
     number.innerText = screenNumbers[screenNumbers.length - 1]
     screen.appendChild(number)
 }
@@ -25,7 +15,24 @@ for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener('click', () => {
         screenNumbers.push(keys[i].innerHTML)
         addNumber()
-        console.log(screenNumbers)
     })
 }
 
+deletKey.addEventListener('click', () => {
+    const deleteNumbers = document.getElementsByClassName('delete')
+    const lastDeleteNumber = deleteNumbers[deleteNumbers.length - 1]
+    lastDeleteNumber.parentNode.removeChild(lastDeleteNumber);
+    screenNumbers.pop()
+    console.log(screenNumbers)
+})
+
+deleteAll.addEventListener('click', () => {
+    const deleteNumbers = document.getElementsByClassName('delete')
+    for (let i = 0; i < screenNumbers.length; i++) {
+        const lastDeleteNumber = deleteNumbers[deleteNumbers.length - 1]
+        lastDeleteNumber.parentNode.removeChild(lastDeleteNumber);
+        console.log(screenNumbers)
+    }
+    screenNumbers = []
+    console.log(screenNumbers)
+})
